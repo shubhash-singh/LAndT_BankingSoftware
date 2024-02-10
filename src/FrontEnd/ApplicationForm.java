67304eedcb5e8618332d7d8a84cf504034d8db2e
Shubhash Singh <subhashsingh2059@gmail.com>
2024-02-10 09:10:34 +0530

    Created Project For L&D 3rd semester

---
+++ src/FrontEnd/ApplicationForm.java
@@ -0,0 +1,100 @@
+package BankingSoftware;
+
+import java.awt.*;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+class ApplicationForm extends JFrame{
+
+    private JLabel bankName;
+    private JLabel name, dob, Address, street, city, Zipcode;
+    private JTextField nameInput, dobInput, AddressInput, streetInput, cityInput, ZipCodeInput;
+
+    ApplicationForm()
+    {
+        bankName = new JLabel("Bank of Chittoor");
+        bankName.setForeground(Color.WHITE);
+        bankName.setFont(new Font("Monotype Corsiva", Font.ITALIC, 55));
+        bankName.setBounds(300, 20, 1000, 80);  //(x, y, width, height)
+        add(bankName);
+
+
+        name = new JLabel("Name: ");
+        name.setForeground(Color.WHITE);
+        name.setFont(new Font("Arial", Font.PLAIN, 28));
+        name.setBounds(580, 150, 200, 30);
+        add(name);
+
+        nameInput = new JTextField();
+        nameInput.setBounds(665, 150, 325, 30);
+        add(nameInput);
+
+        dob = new JLabel("Date of Birth: ");
+        dob.setForeground(Color.WHITE);
+        dob.setFont(new Font("Arial", Font.PLAIN, 28));
+        dob.setBounds(500, 200, 200, 30);
+        add(dob);
+
+        dobInput = new JTextField();
+        dobInput.setBounds(665, 200, 325, 30);
+        add(dobInput);
+
+        Address = new JLabel("Address: ");
+        Address.setForeground(Color.WHITE);
+        Address.setFont(new Font("Arial", Font.PLAIN, 28));
+        Address.setBounds(500, 250, 200, 30);
+        add(Address);
+
+        AddressInput = new JTextField();
+        AddressInput.setBounds(665, 250 ,325, 30);
+        add(AddressInput);
+
+        street = new JLabel("Street: ");
+        street.setForeground(Color.WHITE);
+        street.setFont(new Font("Arial", Font.PLAIN, 28));
+        street.setBounds(500, 300, 200, 30);
+        add(street);
+
+        streetInput = new JTextField();
+        streetInput.setBounds(665, 300, 325, 30);
+        add(streetInput);
+
+        city = new JLabel("City: ");
+        city.setForeground(Color.WHITE);
+        city.setFont(new Font("Arial", Font.PLAIN, 28));
+        city.setBounds(500, 350, 200, 30);
+        add(city);
+
+        cityInput = new JTextField();
+        cityInput.setBounds(665, 350, 325, 30);
+        add(cityInput);
+
+        Zipcode = new JLabel("ZipCode: ");
+        Zipcode.setForeground(Color.WHITE);
+        Zipcode.setFont(new Font("Arial", Font.PLAIN, 28));
+        Zipcode.setBounds(500, 400, 200, 30);
+        add(Zipcode);
+
+        ZipCodeInput = new JTextField();
+        ZipCodeInput.setBounds(665, 400, 325, 30);
+        add(ZipCodeInput);
+
+        // Setting up the background Image
+        ImageIcon backgroundImage = new ImageIcon("/media/ragnar/ca023da0-2328-4858-8f08-a69753e22717/Projects/ProjectsIn2ndYear/BankingSoftware/Data/Images/Login_BackGround2.jpg");
+        Image background = backgroundImage.getImage().getScaledInstance(1000, 650, Image.SCALE_DEFAULT);
+        ImageIcon backgrouIcon2 =new ImageIcon(background);
+        JLabel imagBack = new JLabel(backgrouIcon2);
+        imagBack.setBounds(0, 0, 1000, 650);
+        add(imagBack);
+
+
+        setLayout(null);
+        setSize(1000, 650);
+        setLocation(380, 150);
+        setUndecorated(true);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
---
+++ src/FrontEnd/LoginPage.java
@@ -0,0 +1,125 @@
+package BankingSoftware;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class LoginPage extends JFrame implements ActionListener{
+    private JLabel bankName, alertMsg;
+    private JButton logIn;
+    private JButton signUp;
+    private JLabel accountNo, password;
+    private JTextField accountNoInput;
+    private JPasswordField passwordInput;
+
+    LoginPage() 
+    {
+        // super("Banking Software");
+
+        
+        bankName = new JLabel("Bank of Chittoor");
+        bankName.setForeground(Color.WHITE);
+        bankName.setFont(new Font("Monotype Corsiva", Font.ITALIC, 55));
+        bankName.setBounds(300, 20, 1000, 80);  //(x, y, width, height)
+        add(bankName);
+
+
+        accountNo = new JLabel("Account No: ");
+        accountNo.setForeground(Color.WHITE);
+        accountNo.setFont(new Font("Arial", Font.PLAIN, 28));
+        accountNo.setBounds(500, 150, 200, 30);
+        add(accountNo);
+
+
+        accountNoInput = new JTextField(16);
+        accountNoInput.setBounds(660, 150, 325, 30);
+        add(accountNoInput);
+
+        password = new JLabel("Password:");
+        password.setForeground(Color.WHITE);
+        password.setFont(new Font("Arial", Font.PLAIN, 28));
+        password.setBounds(522, 200, 200, 30);
+        add(password);
+
+
+        passwordInput = new JPasswordField(24);
+        passwordInput.setBounds(660, 200, 325, 30);
+        add(passwordInput);
+
+
+        alertMsg = new JLabel(" ");
+        alertMsg.setFont(new Font("Monotype Corsiva", Font.ITALIC, 16));
+        alertMsg.setForeground(Color.RED);
+        alertMsg.setBounds(650, 250, 300, 30);
+        add(alertMsg);
+
+        logIn = new JButton("Log In");
+        logIn.setForeground(Color.WHITE);
+        logIn.setBackground(Color.RED);
+        logIn.setBorder(null);
+        logIn.addActionListener(this);
+        logIn.setBounds(690, 330, 100, 40);
+        add(logIn);
+
+
+        signUp = new JButton("Create New Account");
+        signUp.setForeground(Color.WHITE);
+        signUp.setBackground(Color.RED);
+        signUp.setBorder(null);
+        signUp.setBounds(640, 400, 200, 40);
+        signUp.addActionListener(this);
+        add(signUp);
+
+
+        // Setting up the background Image
+        ImageIcon backgroundImage = new ImageIcon("/media/ragnar/ca023da0-2328-4858-8f08-a69753e22717/Projects/ProjectsIn2ndYear/BankingSoftware/Data/Images/Login_BackGround2.jpg");
+        Image background = backgroundImage.getImage().getScaledInstance(1000, 650, Image.SCALE_DEFAULT);
+        ImageIcon backgrouIcon2 =new ImageIcon(background);
+        JLabel imagBack = new JLabel(backgrouIcon2);
+        imagBack.setBounds(0, 0, 1000, 650);
+        add(imagBack);
+
+        
+        setLayout(null);
+        setSize(1000, 650);
+        setLocation(380, 150);
+        setUndecorated(true);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+    @Override
+    public  void actionPerformed(ActionEvent ae)
+    {
+        try {
+            
+            if(ae.getSource() == signUp)
+            {
+                new ApplicationForm();
+            }
+
+            else if (ae.getSource() == logIn)
+            {
+                String id = accountNoInput.getText().toString();
+                String Password = passwordInput.getPassword().toString();
+                
+
+                if(id.isEmpty() || Password.isBlank())
+                {
+                    alertMsg.setText("Password");
+                }
+                else
+                {
+                    alertMsg.setText("UserName");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        new LoginPage();
+
+    }
+}
