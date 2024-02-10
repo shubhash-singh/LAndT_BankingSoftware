c3b1458a89d2e0f8a11a90d1793ae9c818c68e3e
Shubhash Singh <subhashsingh2059@gmail.com>
2024-02-10 17:32:19 +0530

    Added Clear Button and Added String to take input from user

--- src/ApplicationForm.java
+++ src/ApplicationForm.java
@@ -1,12 +1,16 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
-class ApplicationForm extends JFrame{
+class ApplicationForm extends JFrame implements ActionListener{
 
     private JLabel bankName;
-    private JLabel name, Mobile, Email, AccType, InitialDeposite;
+    private JLabel name, Mobile, Email, AccType, InitialDeposite, WarningMsg;
     private JTextField nameInput, MobileInput, EmailInput, InitialDepositeInput;
-    private JRadioButton male, female;
+    private JRadioButton current, saving;
+    private JButton Submit, clear;
 
     ApplicationForm()
     {
@@ -16,7 +20,8 @@ class ApplicationForm extends JFrame{
         bankName.setBounds(300, 20, 1000, 80);  //(x, y, width, height)
         add(bankName);
 
-
+        
+        // Name Entry Sections
         name = new JLabel("Name: ");
         name.setForeground(Color.WHITE);
         name.setFont(new Font("Arial", Font.PLAIN, 28));
@@ -27,6 +32,8 @@ class ApplicationForm extends JFrame{
         nameInput.setBounds(665, 150, 325, 30);
         add(nameInput);
 
+
+        // Mobile NUmber Entry Section
         Mobile = new JLabel("Mobile No.: ");
         Mobile.setForeground(Color.WHITE);
         Mobile.setFont(new Font("Arial", Font.PLAIN, 28));
@@ -37,6 +44,8 @@ class ApplicationForm extends JFrame{
         MobileInput.setBounds(665, 200, 325, 30);
         add(MobileInput);
 
+
+        // Email Entry Section
         Email = new JLabel("Email: ");
         Email.setForeground(Color.WHITE);
         Email.setFont(new Font("Arial", Font.PLAIN, 28));
@@ -47,21 +56,30 @@ class ApplicationForm extends JFrame{
         EmailInput.setBounds(665, 250 ,325, 30);
         add(EmailInput);
 
+
+        // Account Type selection section
         AccType = new JLabel("Account Type: ");
         AccType.setForeground(Color.WHITE);
         AccType.setFont(new Font("Arial", Font.PLAIN, 28));
         AccType.setBounds(483, 300, 200, 30);
         add(AccType);
-
-        male = new JRadioButton("Male");
-        female = new JRadioButton("Female");
+        // RadioButton to select Account Type
+        current = new JRadioButton("Current");
+        saving = new JRadioButton("Saving");
+        current.setForeground(Color.BLACK);
+        saving.setForeground(Color.BLACK);
+        current.setFont(new Font("Arial", Font.PLAIN, 28));
+        saving.setFont(new Font("Arial",Font.PLAIN, 28 ));
+        current.setBounds(665, 300, 150, 30);
+        saving.setBounds(820, 300, 150, 30);
         ButtonGroup genderGroup = new ButtonGroup();
-        genderGroup.add(male);
-        genderGroup.add(female);
-        add(male);
-        add(female);
+        genderGroup.add(current);
+        genderGroup.add(saving);
+        add(current);
+        add(saving);
 
 
+        // Initial Deposite section 
         InitialDeposite = new JLabel("Initial Deposite: ");
         InitialDeposite.setForeground(Color.WHITE);
         InitialDeposite.setFont(new Font("Arial", Font.PLAIN, 28));
@@ -71,6 +89,31 @@ class ApplicationForm extends JFrame{
         InitialDepositeInput = new JTextField();
         InitialDepositeInput.setBounds(665, 350, 325, 30);
         add(InitialDepositeInput);
+
+        // Button to submit all the data to open a new Bank account
+        Submit = new JButton("Submit");
+        Submit.setForeground(Color.WHITE);
+        Submit.setBackground(Color.RED);
+        Submit.setBorder(null);
+        Submit.setBounds(650, 450, 170, 40);
+        Submit.addActionListener(this); 
+        add(Submit);
+
+        // Button to clear all the fields
+        clear = new JButton("Clear All");
+        clear.setForeground(Color.WHITE);
+        clear.setBackground(Color.RED);
+        clear.setBorder(null);
+        clear.setBounds(650, 500, 170, 40);
+        clear.addActionListener(this); 
+        add(clear);
+
+        // Warning Message
+        WarningMsg = new JLabel();
+        WarningMsg.setFont(new Font("Monotype Corsiva", Font.ITALIC, 16));
+        WarningMsg.setForeground(Color.RED);
+        WarningMsg.setBounds(getBounds());
+        add(WarningMsg);
 
         // Setting up the background Image
         ImageIcon backgroundImage = new ImageIcon("//media/ragnar/ca023da0-2328-4858-8f08-a69753e22717/Projects/L-T_BankingSoftware/src/Data/Images/Login_BackGround2.jpg");
@@ -88,8 +131,30 @@ class ApplicationForm extends JFrame{
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-    public static void main(String[] args) {
-        new ApplicationForm();
+    public void actionPerformed(ActionEvent ae) {
+        try {
+            if(ae.getSource() == Submit) {
+                String Name = nameInput.getText().toString();
+                String Mobile_No = MobileInput.getText().toString();
+                String Email_Id = EmailInput.getText().toString();
+                String Initial_Deposite =InitialDepositeInput.getText().toString();
+
+                if(Name.isBlank() || Mobile_No.isBlank() || Email_Id.isBlank()) {
+
+                }
+                else if(Integer.parseInt(Initial_Deposite) < 0 || Initial_Deposite.isBlank()) {
+
+                }
+            }
+            else if (ae.getSource() == clear) {
+                nameInput.setText("");
+                MobileInput .setText("");
+                EmailInput.setText("");
+                InitialDepositeInput.setText("");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     
 }
